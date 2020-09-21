74937d6630718270a35c7105a2d952a7c46e06c3
heavenbeing <heavenbeing@outlook.com>
2020-09-21 15:43:48 +0800

    修改邮件解析地址

--- jobs/send_email_job.py
+++ jobs/send_email_job.py
@@ -25,10 +25,10 @@ print(response.text)
 
 # /stock/data?table_name=guess_indicators_lite_buy_daily
 # /stock/data?table_name=guess_indicators_lite_sell_daily
-email_content = "Ip address:" + response.text.replace('\n', '').replace('\r', '') + "\n" \
-                + "stock address:" + "http://" + response.text.replace('\n', '').replace('\r', '') + ":9999\n" \
-                + "买入猜想(超买):" + "http://" + response.text.replace('\n', '').replace('\r', '') + ":9999" + "/stock/data?table_name=guess_indicators_lite_buy_daily\n" \
-                + "卖出猜想(超卖):" + "http://" + response.text.replace('\n', '').replace('\r', '') + ":9999" + "/stock/data?table_name=guess_indicators_lite_sell_daily\n" \
+email_content = "Ip address:" + "heavenbeing.tpddns.cn\n" \
+                + "stock address:" + "http://heavenbeing.tpddns.cn:9999/\n" \
+                + "买入猜想(超买):" + "http://heavenbeing.tpddns.cn:9999/stock/data?table_name=guess_indicators_lite_buy_daily\n" \
+                + "卖出猜想(超卖):" + "http://heavenbeing.tpddns.cn:9999/stock/data?table_name=guess_indicators_lite_sell_daily\n" \
                 + " \r\n" \
                 + " \r\nsend by python"
 msg = MIMEText(email_content, 'plain', 'utf-8')
